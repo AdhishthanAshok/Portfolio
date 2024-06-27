8377e5bf158eb87900fce31ddb5cd4f782f8f890
Adhishthan <adhishthanashok@gmail.com>
2024-06-27 23:16:46 +0530

    Changed Portfolio Design

--- my_portfolio/src/App2.jsx
+++ my_portfolio/src/App2.jsx
@@ -1,57 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import Home from "./components/Home.jsx";
-// import Projects from "./components/Projects";
-// import AboutMe from "./components/AboutMe";
-// import Resume from "./components/Resume";
-// import ContactMe from "./components/ContactMe";
-// import Theme from "./components/Theme.jsx";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App pt-3 dark:bg-gray-900 transition-colors duration-300">
-//         <nav className="mb-4 space-x-2 flex justify-center text-gray-900 dark:text-white ">
-//           <Link to="/">
-//             <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
-//               Home
-//             </button>
-//           </Link>
-//           <Link to="/projects">
-//             <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
-//               Projects
-//             </button>
-//           </Link>
-//           <Link to="/about-me">
-//             <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
-//               About Me
-//             </button>
-//           </Link>
-//           <Link to="/resume">
-//             <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
-//               Resume
-//             </button>
-//           </Link>
-//           <Link to="/contact-me">
-//             <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
-//               Contact Me
-//             </button>
-//           </Link>
-//           <Theme />
-//         </nav>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/about-me" element={<AboutMe />} />
-//           <Route path="/resume" element={<Resume />} />
-//           <Route path="/contact-me" element={<ContactMe />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home.jsx";
@@ -60,7 +6,6 @@ import AboutMe from "./components/AboutMe";
 import Resume from "./components/Resume";
 import ContactMe from "./components/ContactMe";
 import Theme from "./components/Theme.jsx";
-import header_logo from "./assets/AA.webp";
 
 function App() {
   return (
@@ -72,10 +17,21 @@ function App() {
               to="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
-              <img src={header_logo} className="h-8" alt="Flowbite Logo" />
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="h-8"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                MyPortfolio
+              </span>
             </Link>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-              <Theme />
+              <Link to="/">
+                <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-800">
+                  Home
+                </button>
+              </Link>
               <button
                 data-collapse-toggle="navbar-cta"
                 type="button"
--- my_portfolio/src/components/Home.jsx
+++ my_portfolio/src/components/Home.jsx
@@ -1,38 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Typewriter from "typewriter-effect";
 import profile_pic from "../assets/pic1.png";
-import { Link } from "react-router-dom";
+import leetcode_image from "../assets/LC.png";
 
 const Home = () => {
   return (
-    <div className="smooth-scroll">
-      <div
-        className={`min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900 transition-colors duration-300`}
-      >
+    <div className="smooth-scroll flex flex-col items-center">
+      <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900 transition-colors duration-300">
         <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-4xl p-5 md:p-10">
           <div className="flex-1 text-center md:text-left mb-8 md:mb-0">
-            <h1 className="text-2xl md:text-2xl font-bold text-gray-900 dark:text-white inline-block">
-              Hello , I'm {"\u00A0"}
+            <h1 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white inline-block">
+              Hello, I'm{"\u00A0"}
             </h1>
-            <h1 className="text-2xl md:text-3xl font-bold text-indigo-600 dark:text-indigo-600 ">
-              <Typewriter
-                options={{
-                  strings: [" Adhishthan Ashok"],
-                  autoStart: true,
-                  loop: true,
-                  delete: false,
-                }}
-              />
+            <h1 className="text-2xl md:text-2xl font-bold text-indigo-600 dark:text-indigo-600">
+              Adhishthan Ashok
             </h1>
-            <h1 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white ">
+            <h1 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
               I'm a{"\u00A0"} <br />
             </h1>
-            <h1 className="text-2xl md:text-3xl font-bold text-indigo-600 dark:text-indigo-600 inline-block">
+            <h1 className="text-2xl md:text-2xl font-bold text-indigo-600 dark:text-indigo-600 inline-block">
               <Typewriter
                 options={{
                   strings: [
                     "Full Stack Web Developer",
-                    "Open Source Contributer",
+                    "Open Source Contributor",
                     "Tech Enthusiast",
                   ],
                   autoStart: true,
@@ -45,8 +36,20 @@ const Home = () => {
           <div className="flex-1 flex items-center justify-center">
             <img
               src={profile_pic}
-              alt="Placeholder"
-              className="rounded-full w-60 h-60 md:w-80 md:h-80 border-4 border-white dark:border-gray-900"
+              alt="Profile"
+              className="rounded-full w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 border-4 border-white dark:border-gray-900"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full">
+          <h1 className="text-3xl font-bold text-center py-8 dark:text-white">
+            Days, I code
+          </h1>
+          <div className="w-full max-w-md md:max-w-4xl ">
+            <img
+              className="h-auto w-full mx-auto transition-all duration-500 rounded-lg blur-sm hover:blur-none"
+              src={leetcode_image}
+              alt="Leetcode activity"
             />
           </div>
         </div>
