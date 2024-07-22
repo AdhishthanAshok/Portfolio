cbd9458bfa476f9f0a0393b87c2d16f4ce477f21
Adhishthan <adhishthanashok@gmail.com>
2024-07-22 22:29:46 +0530

    Added Placeholder name for contact section

--- my_portfolio/src/components/ContactMe.jsx
+++ my_portfolio/src/components/ContactMe.jsx
@@ -142,7 +142,7 @@ const ContactMe = () => {
                     <input
                       type="text"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
-                      placeholder="John"
+                      placeholder="Bill"
                       name="user_name"
                       required
                     />
